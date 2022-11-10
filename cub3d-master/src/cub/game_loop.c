cabb5f0fd46c29af84d12b859551413c46a95b48
Leticia <canumsang@gmail.com>
2022-11-10 02:36:25 -0300

    adds rays_struct_init function

--- cub3d-master/src/cub/game_loop.c
+++ cub3d-master/src/cub/game_loop.c
@@ -74,11 +74,9 @@ int draw_line(t_map *map, int color)
 	return(0);
 }
 
-float dist (float ax, float ay, float bx, float by)
+float dist (float beginX, float beginY, float endX, float endY)
 {
-
-	return (sqrt((bx - ax) * (bx - ax) + (by -ay) * (by - ay)));
-
+	return (sqrt((endX - beginX) * (endX - beginX) + (endY - beginY) * (endY - beginY)));
 }
 
 void draw_rays(t_map *map)
@@ -153,10 +151,10 @@ void draw_rays(t_map *map)
 		map->rays.dist_vertical = 1000000;
 		vx = map->rays.pos_x;
 		vy = map->rays.pos_y;
-		map->rays.neg_tan =-tan(map->rays.ray_angle);
+		map->rays.neg_tan = -tan(map->rays.ray_angle);
 		if(map->rays.ray_angle > P2 || map->rays.ray_angle < P3) //olhando lado esquerdo
 		{
-			map->rays.ray_x = (((int)map->rays.pos_x >> 5) << 5)-0.0001;
+			map->rays.ray_x = (((int)map->rays.pos_x >> 5) << 5) - 0.0001;
 			map->rays.ray_y = (map->rays.pos_x - map->rays.ray_x) * map->rays.neg_tan + map->rays.pos_y;
 			map->rays.step_x = -32;
 			map->rays.step_y = -map->rays.step_x * map->rays.neg_tan;
@@ -220,7 +218,8 @@ void	paint_gamer(t_map *map)
 	int pixel_vertical;//y;
 
 	map->gamer.ptr_img = mlx_new_image(map->mlx.mlx_ptr, 8, 8);
-	map->gamer.data = (int *)mlx_get_data_addr(map->gamer.ptr_img, &map->gamer.bpp, &map->gamer.line_size, &map->gamer.endian);
+	map->gamer.data = (int *)mlx_get_data_addr(map->gamer.ptr_img,
+		&map->gamer.bpp, &map->gamer.line_size, &map->gamer.endian);
 	pixel_horizontal = 0;
 	while (pixel_horizontal < 8)
 	{
@@ -232,7 +231,8 @@ void	paint_gamer(t_map *map)
 		}
 		pixel_horizontal++;
 	}
-	mlx_put_image_to_window(map->mlx.mlx_ptr, map->mlx.win, map->gamer.ptr_img, map->rays.pos_x, map->rays.pos_y);
+	mlx_put_image_to_window(map->mlx.mlx_ptr, map->mlx.win,
+		map->gamer.ptr_img, map->rays.pos_x, map->rays.pos_y);
 	//draw_line(mlx_ptr, win, px+4, py+4, px+pdx*5, py+pdy*5, 0xFF6347);
 	draw_rays(map);
 }
@@ -283,7 +283,8 @@ void	color_background(t_map *map) //pintar a tela
     int rows;
 
 	map->back.ptr_img = mlx_new_image(map->mlx.mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT);
-    map->back.data = (int *)mlx_get_data_addr(map->back.ptr_img, &map->back.bpp, &map->back.line_size, &map->back.endian);
+    map->back.data = (int *)mlx_get_data_addr(map->back.ptr_img, &map->back.bpp,
+		&map->back.line_size, &map->back.endian);
     while (++columns < SCREEN_HEIGHT)
     {
         rows = -1;
@@ -344,21 +345,26 @@ int	event_key(int keycode, t_map *map)
 	return 0;
 }
 
+void	rays_struct_init(t_map *map)
+{
+	map->rays.pos_x = 40;
+	map->rays.pos_y = 80;
+	map->rays.map_x = map->width;
+	map->rays.map_y = map->height;
+	map->rays.gamer_angle = PI;
+	map->rays.deltax = cos(map->rays.gamer_angle) * 5;
+	map->rays.deltay = sin(map->rays.gamer_angle) * 5;
+}
+
 void	game_loop(t_map *map)
 {
 	// int	x;
 	// int	y;
 	// x = map->width * 32;
 	// y = map->height * 32;
-	map->rays.pos_x = 40;
-	map->rays.pos_y = 80;
 	// printf("coordenadas player: %f e %f\n", map->rays.pos_x, map->rays.pos_y);
-	map->rays.map_x = map->width;
-	map->rays.map_y = map->height;
-	map->rays.gamer_angle = PI;
 	// map_x e map_y é pra ser a largura e altura do mapa em celulas? (width e height, entao?)
-	map->rays.deltax = cos(map->rays.gamer_angle) * 5;
-	map->rays.deltay = sin(map->rays.gamer_angle) * 5;
+	rays_struct_init(map);
 	map->mlx.mlx_ptr = mlx_init();
 	map->mlx.win = mlx_new_window(map->mlx.mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT, "cub3d");
 	color_background(map);
